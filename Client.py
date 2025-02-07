7ddc3822b43ce8bc17d02e5f24e9af183acb822b
DarienBiliak <dbiliak04@gmail.com>
2025-02-07 16:21:33 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,26 +1,49 @@
-#Imports
+#Code Before Change
+"""
+import socket
+
+PORT = 12345
+
+def start_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', PORT))
+    client_name = client_socket.recv(1024).decode()
+    print(f"Connected to server as {client_name}.")
+
+    while True:
+        message = input(f"[{client_name}] Enter message: ")
+        client_socket.send(message.encode())
+
+        if message == "exit":
+            break
+        response = client_socket.recv(1024).decode()
+        print(f"[Server] {response}")
+
+        
+    client_socket.close()
+    print("Disconnected from server.")
+
+if __name__ == '__main__':
+    start_client() 
+"""
+#Code After Change
 from socket import *
 
 serverName = "localhost"
 serverPort = 63464
 
-try:
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
 
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
+while True:
+    userInput = input("Input a lowercase sentence: ")
+    clientSocket.send(userInput.encode())
 
-    while True:
-        user = input("Input a lowercase sentence: ")
-        clientSocket.send(user.encode())
+    if userInput.lower() == "exit":
+        break
 
-        if user.lower() == "exit":
-            break
-
-        data = clientSocket.recv(1024).decode()
-        print("From Server:", data)
-        
-except Exception as e:
-    print(f"An error occurred: {e}")
-finally:
-    clientSocket.close()
-    print("Connection closed.")
+    data = clientSocket.recv(1024).decode()
+    print("From Server:", data)
+    
+clientSocket.close()
+print("Disconnected from server.")
